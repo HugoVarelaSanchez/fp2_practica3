9eb0cb498c691575b618a0fe60bd6ca08795d6f3
Hugo <hugo.varela.sanchez@udc.es>
2024-04-07 02:31:40 +0200

    Estan en la carpeta los archivos que se van a utilizar, e importados. Trota necesito hablar con el de programacion pa entender que hay que hacer ajajaj

---
+++ codigo/main.py
@@ -0,0 +1,13 @@
+#Hugo Varela Sanchez; hugo.varela.sanchez@udc.es
+#David Fernandez Reimundez; david.fernandez.reimundez@udc.es
+
+#Importamos la libreris sys, y los archivos para implementar el TAD Lista Posicional Ordenada
+import sys
+import array_ordered_positional_list.py
+import linked_ordered_positional_list.py
+
+
+#archivo = sys.argv[1]
+archivo = peliculas.txt
+
+
